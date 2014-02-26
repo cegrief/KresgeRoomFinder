a4c455870d966f5640209ecd56f628f8353eed13
Thomas M. Saxton <thomasmsaxton@gmail.com>
2014-02-26 13:22:42 -0600

    Started Canvas

--- includes/js/kresge.js
+++ includes/js/kresge.js
@@ -1,3 +1,22 @@
+function resizeMap(){
+	canvas = $('#map');
+	canvas.width(window.innerWidth);
+	canvas.height(window.innerHeight-147);
+};
+
+function drawRoomToRoom(){
+	var c = document.getElementById("map");
+	var ctx = c.getContext("2d");
+	ctx.strokeStyle="#FF0000";
+	ctx.beginPath();
+	ctx.lineCap="round";
+	ctx.lineWidth=10;
+	ctx.moveTo(93,456);
+	ctx.lineTo(193,456);
+	ctx.stroke();
+
+}
+
  $(document).ready(function() {
  	$('.subject').click(function(){
  		var output = $(this).html();
@@ -7,5 +26,10 @@
  	$("#classSubmit").click(function(){
  		
  	})
+	
+	drawRoomToRoom();
+	resizeMap();
+
+	window.addEventListener('resize', resizeMap, false); // will resize map canvas when window size changes
 
  });
