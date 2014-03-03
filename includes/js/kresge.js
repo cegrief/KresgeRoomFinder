e70b03445b0ffa842cd2e4586364959bcf505212
Thomas M. Saxton <thomasmsaxton@gmail.com>
2014-03-03 08:46:40 -0600

    Added real data to class/staff search

--- includes/js/kresge.js
+++ includes/js/kresge.js
@@ -1,6 +1,25 @@
 // Global Variables
-var classes = new Array(['101 (Smith)', '101 (Jones)', '105 (Henry)'], ['101 (Friedrich)', '110 (Grief)', '115 (Saxton)', '220 (Simon)'], ['101 (Stein)', '101 (Pechter)', '101 (Felton)', '310 (Hi)'], ['101 (Google)', '215 (Kresge)', 'Purple Monkey Dishwasher']);
-var depts = new Array('English', 'German', 'Spanish', 'Swahili');
+var classes = new Array(
+		['230-60-DIS (Staff)', '230-61-DIS (Staff)', '230-62-DIS (Staff)', '230-63-DIS (Staff)', '355-20-SEM (Smith)', '375-20-LEC (Hesse)', '381-20-LEC (Johnson)', '381-21-LEC (Burchell)', '394-20-LEC (King)'],
+		['310-22-SEM (Gealy)'],
+		['378-20-LEC (Hoffman)'],
+		['111-3-20-LEC (Whitcomb)', '111-3-21-LEC (Whitcomb)', '111-3-21 (Horesh)', '114-2-LEC (Mikhaeel)', '121-3-20-LEC (Khan)', '121-3-22-LEC (Khan)', '211-3-20-LEC (Khan)', '311-3-20-LEC (Mihkaeel)'],
+		['120-21-LEC (Falkowski)', '120-22-LEC (Falkowski)', '120-23-LEC (Falkowski)', '120-24-LEC (Falkowski)', '125-20-LEC (Sokolow)', '125-21-LEC (Sokolow)', '125-22-LEC (Sokolow)', '140-21-LEC (Kaczynski)', '140-22-LEC (Knezevic)', '150-20-LEC (Bannos)', '150-21-LEC (Bannos)', '230-20-LEC (Ovalle)', '252-64-LEC (Bannos)', '260-20-LEC (Reinke)', '272-20-LEC (Reinke)','322-2-20-LEC (Legerwood)', '380-20-LEC (Legerwood)', '390-20-LEC (Bannos)', '390-21-LEC (Knezevic)'],
+		['385-20-LEC (Thompson)', '390-20-SEM (Bell)'],
+		['230-60-DIS (Staff)', '230-61-DIS (Staff)', '230-62-DIS (Staff)', '230-63-DIS (Staff)', '392-22-LEC (Enteen)'],
+		['239-20-LEC (Pennington)', '394-26-LEC (Staff)'],
+		['CFS 392-21-SEM (Taylor)', '395-1-21-SEM (Butler)', '395-2-21-SEM (Butler)', '398-21-SEM (Staff)'],
+		['111-3-21-LEC (Ji)', '111-3-22-LEC (Jiang)', '115-3-20-LEC (Luo)', '115-3-21-LEC (Luo)', '121-3-20-LEC (Sun)', '121-3-21-LEC (Wang)', '121-3-22-LEC (Sun)', '121-3-23-LEC (Sun)', '125-3-21-LEC (Robertson)', '211-3-20-LEC (Chang)', '211-3-21-LEC (Chang)', '311-3-20-LEC (Hsieh)', '311-3-21-LEC (Chang)', '315-3-20-LEC (Gu)'],
+		['212-60-DIS (Staff)', '212-61-DIS (Staff)', '212-62-DIS (Staff)', '212-63-DIS (Staff)', '212-64-DIS (Staff)', '260-60-DIS (Staff)', '260-61-DIS (Staff)', '260-62-DIS (Staff)', '260-63-60 (Staff)', '260-64-DIS (Staff)', '320-20-LEC (Wallace)', '350-20-LEC (Hopman)'],
+		['396-20-SEM (Mastronardi)', '396-21-SEM (Kelso)'],
+		['215-20-LEC (Ray)', '322-20-LEC (Cobb)'],
+		['211-21-LEC (Brueck)', '211-60-DIS (Cavanagh)', '274-3-20-LEC (Shen)', '301-20-LEC (Hopman)', '304-21-LEC (Zaperini)', '304-22-LEC (Marti-Lopez)', '383-20-LEC (Deutscher)', '383-60-DIS (Staff)', '383-62-DIS (Staff)'],
+		['335-21-LEC'],
+		['201-52-DIS (Witte)', '201-55-DIS (Witte)', '202-51-DIS (Kiesling)', '202-54-DIS (Kiesling)', '202-62-DIS (Ogawa)', '281-21-DIS (Walker)', '281-24-DIS (Walker)', '281-25-DIS (Walker)', '281-26-DIS (Walker)', '310-1-21-DIS (Schulz)', '310-1-24-DIS (Schulz)', '310-1-25-DIS (Schulz)', '310-2-21-DIS (Ely)', '325-20-LEC (Matsuyama)', '398-2-20-LEC (Hornsten)'],
+		['EECS 203-01-LEC (Zhou)'],
+		['101-6-22-LEC (Krienke)', '105-6-21-LEC (Hirsch)', '105-6-22-LEC (Herrick)', '205-22-LEC (Gealy)', '270-2-63-DIS (Staff)', '270-2-64-DIS (Staff)', '385-21-LEC (Froula)', '395-5-20-LEC (Biss)'],
+		['390-23-LEC (Woodhouse)']);
+var depts = new Array('AM_AM_ST', 'AMER_ST', 'ANTHRO', 'ARABIC', 'ART', 'ART_HIST', 'ASIAN_AM', 'BUS_INST', 'CFS', 'CHINESE', 'CLASSICS', 'CMN', 'COMM_ST', 'COMP_LIT', 'DANCE', 'ECON', 'EECS', 'ENGLISH', 'ENVR_POL');
 var startingFloor = 0;
 var goalFloor = 0;
 var activeSearch = '';
@@ -350,6 +369,18 @@ $(".exita").click(function(){
 
 	resizeMap();
 	window.addEventListener('resize', resizeMap, false); // will resize map canvas when window size changes
+
+	var deptSelect = $('#dept');
+	deptSelect.empty();
+	for (var x in depts){
+		deptSelect[0].add(new Option(depts[x], depts[x]));
+	}
+	var select = $("#classnum");
+	select.empty();
+	var bus = classes[0];
+	for (var x in bus){
+		select[0].add(new Option(bus[x], bus[x]));
+	}
 
 	$("#dept").change(function(){
 		var dept = $("#dept option:selected").attr('value');
